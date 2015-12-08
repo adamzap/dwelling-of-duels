8b141effded05a024fe32317b215fdea5e8f3a0d
Adam Zapletal <adamzap@gmail.com>
2015-12-07 19:55:13 -0600

    Normalize strings before searching

--- templates/static/js/make-filter.js
+++ templates/static/js/make-filter.js
@@ -1,3 +1,7 @@
+function normalize (s) {
+  return s.replace('š', 's').replace('é', 'e');
+}
+
 function make_filter() {
   var input = document.getElementById('filter');
 
@@ -6,12 +10,14 @@ function make_filter() {
   });
 
   input.addEventListener('input', function (e) {
-    var query = e.target.value.toLowerCase();
+    var query = normalize(e.target.value.toLowerCase());
     var rows = document.querySelectorAll('tbody tr');
     var length = rows.length;
 
     for (var i = 0; i < length; i++) {
       var s = rows[i].querySelectorAll('a')[0].textContent.toLowerCase();
+
+      s = normalize(s);
 
       rows[i].style.display = s.indexOf(query) !== -1 ? 'table-row' : 'none';
     }
