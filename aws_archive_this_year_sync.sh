83ea33ba5692ab3072ac3b6ef77c57628c6899f3
Austin Green <austinjgreen94@yahoo.com>
2023-03-16 10:55:58 -0400

    amend this_year script excludes

--- aws_archive_this_year_sync.sh
+++ aws_archive_this_year_sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-aws s3 sync ./dodarchive s3://dwellingofduels-static-site/dodarchive --delete --cache-control max-age=604800 --exclude "0?-??-*/*" --exclude "1?-??-*/*" --exclude "20-??-*/*" --exclude "21-??-*/*"
+aws s3 sync ./dodarchive s3://dwellingofduels-static-site/dodarchive --delete --cache-control max-age=604800 --exclude "0?-??-*/*" --exclude "1?-??-*/*" --exclude "20-??-*/*" --exclude "21-??-*/*" --exclude "22-??-*/*"
 
 ./aws_cloudfront_invalidate.sh
