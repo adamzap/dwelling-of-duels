d340b190bd1d7e156a2fd2afc43170a7c0d295d3
Austin Green <austinjgreen94@yahoo.com>
2023-03-29 22:29:49 -0400

    usr/bin/env

--- aws_archive_this_year_sync.sh
+++ aws_archive_this_year_sync.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 aws s3 sync ./dodarchive s3://dwellingofduels-static-site/dodarchive --delete --cache-control max-age=604800 --exclude "0?-??-*/*" --exclude "1?-??-*/*" --exclude "20-??-*/*" --exclude "21-??-*/*" --exclude "22-??-*/*"
 
 ./aws_cloudfront_invalidate.sh
--- aws_html_sync.sh
+++ aws_html_sync.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/usr/bin/env bash
 aws s3 sync ./deploy s3://www.dwellingofduels.net --delete --cache-control max-age=604800
