2a9cff7012e3742a9f28c9d32b3f522a7190866e
Austin Green <austinjgreen94@yahoo.com>
2023-03-30 11:19:00 -0400

    add 'set -o errexit' to bash scripts

--- aws_archive_sync.sh
+++ aws_archive_sync.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -o errexit
 aws s3 sync ./dodarchive s3://dwellingofduels-static-site/dodarchive --delete --cache-control max-age=604800
 
 ./aws_cloudfront_invalidate.sh
--- aws_cloudfront_invalidate.sh
+++ aws_cloudfront_invalidate.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+
+set -o errexit
 aws cloudfront create-invalidation --distribution-id=E1VLCEEPT618YJ --paths="/*"
