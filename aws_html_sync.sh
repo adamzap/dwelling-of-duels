c3c533e41bbc69466e59015d2da7bf91188c4f06
Austin Green <austinjgreen94@yahoo.com>
2023-05-30 22:59:11 -0400

    html sync size only

--- aws_html_sync.sh
+++ aws_html_sync.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 set -o errexit
-aws s3 sync ./deploy s3://www.dwellingofduels.net --delete --cache-control max-age=604800
+aws s3 sync ./deploy s3://www.dwellingofduels.net --delete --cache-control max-age=604800 --size-only
